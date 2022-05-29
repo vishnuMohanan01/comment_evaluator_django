d57bcd27b1d0912e12519e32ab0eb2416c93dffa
Iam-VM <vishnumohanantheking@gmail.com>
2022-05-29 22:13:10 +0530

    added easyocr and image evaluate handling

--- evaluator_api/views.py
+++ evaluator_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from scipy.special import softmax
+import easyocr
 
 
 # Create your views here.
@@ -31,5 +32,27 @@ def evaluate_text(req):
             return Response({"status": "not ok", "comment": req.POST.get("comment")}, status=status.HTTP_200_OK)
 
 
+@api_view(['POST'])
 def evaluate_image(req):
-    pass
+    if req.method == 'POST':
+        evaluator = evaluator_api.utils.load_eval_model.get_img_eval_model()
+
+        tokenizer = evaluator["tokenizer"]
+        model = evaluator["model"]
+
+        img = req.FILE("image")
+        reader = easyocr.Reader(['en'])
+        results = reader.readtext(img)
+        text = ''
+        for result in results:
+            text += result[1] + ' '
+
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+
+        if scores[0] > 0.5:
+            return Response({"status": "ok", "comment": req.POST.get("comment")}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "not ok", "comment": req.POST.get("comment")}, status=status.HTTP_200_OK)
