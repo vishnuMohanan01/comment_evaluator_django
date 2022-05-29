39da91cc2cdfb9cac81a8dd17eb746b914b58415
Iam-VM <vishnumohanantheking@gmail.com>
2022-05-29 21:39:29 +0530

    fetching allowed hosts from .env

--- comment_evaluator/settings.py
+++ comment_evaluator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = list(env('ALLOWED_HOSTS'))
 
 
 # Application definition
